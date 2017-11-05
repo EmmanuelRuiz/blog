d9ae77e0f413fd145725a4972412fda3965f32ce
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-11-04 20:41:05 -0600

    obtener objetos

--- symfony/src/BlogBundle/Controller/DefaultController.php
+++ symfony/src/BlogBundle/Controller/DefaultController.php
@@ -8,6 +8,17 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        //enlistar entradas
+        $em = $this->getDoctrine()->getManager();
+        $entry_repo = $em->getRepository("BlogBundle:Entry");
+        $entries = $entry_repo->findAll();
+        //imprimir entrada, categoria y nombre de usuario
+        foreach ($entries as $entry){
+            echo $entry->getTitle()."<br>";
+            echo $entry->getCategory()->getName()."<br>";
+            echo $entry->getUser()->getName()."<br><hr>";
+        }
+        die();
         return $this->render('BlogBundle:Default:index.html.twig');
     }
 }
