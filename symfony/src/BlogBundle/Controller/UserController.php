829b8f1506b937b92d053d6d8643a811a1472c5c
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-11-20 22:49:57 -0600

    modulo de registrar usuario

--- symfony/src/BlogBundle/Controller/UserController.php
+++ symfony/src/BlogBundle/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BlogBundle\Entity\User;
+use BlogBundle\Form\UserType;
 
 class UserController extends Controller
 {
@@ -12,9 +14,36 @@ class UserController extends Controller
         $authenticationUtils = $this->get("security.authentication_utils");
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
+        
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $user = new User();
+            $user->setName($form->get("name")->getData());
+            $user->setSurname($form->get("surname")->getData());
+            $user->setEmail($form->get("email")->getData());
+            $user->setPassword($form->get("password")->getData());
+            $user->setRole("ROLE_USER");
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $flush = $em->flush();
+            
+            if($flush == null){
+                $status = "Usuario creado correctamente";    
+            } else {
+                $status = "No se pudo crear el usuario";
+            }
+             
+        } else {
+            $stauts = "No se pudo registrar el usuario";
+        }
+        
+        
         return $this->render("BlogBundle:user:login.html.twig", array(
             "error" => $error,
-            "last_username" => $lastUsername
+            "last_username" => $lastUsername,
+            "form" => $form->createView()
         ));
     }
 }
